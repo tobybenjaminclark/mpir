4e16819b9ea6418341fcbd329719680cb53170ce
Toby Clark <psytc9@nottingham.ac.uk>
2023-07-11 23:05:15 +0100

    add: basic string language parsing

--- Codebank/tokenizer.c
+++ Codebank/tokenizer.c
@@ -1,41 +1,119 @@
-#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
+// Grammar Definition
+// statement            ::=   string_definition | string_expression
+// string_definition    ::=   'string' variable_name string_literal
+// string_expression    ::=   string_expression '+' string | string
+// string               ::=   char | string char
+
+void tokenize_string();
+void tokenize_variable_name();
+void tokenize_string_literal();
+void tokenize_string_expression();
+void tokenize_string_definition();
+void tokenize_statement();
+void tokenize_consume_character(char expected);
+
+// global declaration
+char* input;
+
+void tokenize_consume_character(char expected)
 {
-  g_print ("Hello World\n");
+	if (*input == expected)
+	{
+		input++;
+	}
+	else
+	{
+		printf("error: Unexpected character %c \n", *input);
+		exit(1);
+	}
 }
 
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
+void tokenize_string()
 {
-  GtkWidget *window;
-  GtkWidget *button;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  gtk_window_set_child (GTK_WINDOW (window), button);
-
-  gtk_window_present (GTK_WINDOW (window));
+	printf("tokenize_string: %s \n", input);
+	int alpha_contender = isalpha(*input);
+    while (alpha_contender)
+	{
+		printf("tokenize_string: %c \t:\t %d \n", *input, alpha_contender);
+        input++;
+		alpha_contender = isalpha(*input);
+    }
 }
 
-int
-main (int    argc,
-      char **argv)
+int main()
 {
-  GtkApplication *app;
-  int status;
+    // Taking Input
+    char line_one[50];
+    printf("Type line 1\n");
+    fgets(line_one, sizeof(line_one), stdin);
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+    line_one[strcspn(line_one, "\n")] = '\0'; // Remove newline character
 
-  return status;
+    input = line_one;  // Assign the input pointer to line_one
+
+    printf("input is '%s'\n", input);
+    printf("first character is '%c' \n", *input);
+
+    tokenize_statement();
+
+    return 0;
+}
+
+void tokenize_variable_name()
+{
+	printf("tokenize_variable_name: %s \n", input);
+	tokenize_string();
+}
+
+void tokenize_string_literal()
+{
+	printf("tokenize_string_literal: %s \n", input);
+	tokenize_consume_character('"');
+	tokenize_string();
+	tokenize_consume_character('"');
+}
+
+// string_expression    ::=   string_expression '+' string | string
+void tokenize_string_expression()
+{
+	printf("tokenize_string_expression: %s \n", input);
+	tokenize_string();
+	while(*input == '+')
+	{
+		tokenize_consume_character('+');
+		tokenize_string();
+	}
+}
+
+void tokenize_string_definition()
+{
+	printf("tokenize_string_definition: %s \n", input);
+	tokenize_consume_character('s');
+	tokenize_consume_character('t');
+	tokenize_consume_character('r');
+	tokenize_consume_character('i');
+	tokenize_consume_character('n');
+	tokenize_consume_character('g');
+	tokenize_consume_character(' ');
+	tokenize_variable_name();
+	tokenize_consume_character(' ');
+	tokenize_string_literal();
+}
+
+void tokenize_statement()
+{
+	printf("tokenize_statement: %s \n", input);
+	if (*input == 's')
+	{
+		tokenize_string_definition();
+	}
+	else
+	{
+		tokenize_string_expression();
+	}
 }
