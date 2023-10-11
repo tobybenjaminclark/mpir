421b02e785272ceab77fb0b520f753fdf47d2833
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-11 15:38:23 +0100

    add: function definition & declaration to cfg.lua

--- codebank/cfg.lua
+++ codebank/cfg.lua
@@ -1,6 +1,13 @@
 
+-- widely-used declarations
+letter                      ::=         'a' | 'b' | ... | 'z' | 'A' | 'B' | ... | 'Z'
+digit                       ::=         '0' | '1' | ... | '9'
+
+-- function
 function_declaration        ::=         "func " function_identifier "::" function_curry
 function_identifier         ::=         ( letter | digit | symbol ) | ( identifier letter | digit | symbol)
 function_curry              ::=         ( identifier ("->"|"→") identifier | function_curry)                              -- identifier here must be a type.
-letter                      ::=         'a' | 'b' | ... | 'z' | 'A' | 'B' | ... | 'Z'
-digit                       ::=         '0' | '1' | ... | '9'
+
+function_definition         ::=         "func " function_identifier parameter_list
+parameter                   ::=         ( letter | digit | symbol ) | ( identifier letter | digit | symbol)
+parameter_list              ::=         parameter | (parameter parameter_list)
