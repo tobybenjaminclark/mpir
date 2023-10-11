93da7bbb94587c4ac1e5451c7578c3292b61912e
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-11 14:05:43 +0100

    add: changes to cfg file, finalizing syntax design

--- codebank/context-free-grammar.lua
+++ codebank/context-free-grammar.lua
@@ -31,7 +31,7 @@ number                  ::= digit | ( number digit )
 -- x is in a superposition > 2 and < 3 but is not known until observed (read from memory)
 -- x: int | { x > 1 } | { x < 5 }
 
--- maybe use feta or other symbol to represent x?
+    -- maybe use feta or other symbol to represent x?
 
 -- contextual type annotations
 -- Can extend types to add relevant stuff for that variable e.g.
@@ -97,5 +97,55 @@ set x as x + 1
 --      | doc a as "natural-number"`
 -- endtype
 
---
+-- boolean checking of ==
+-- could be 'is'
+
+-- Natural Language Syntax
+-- if x is True:
+--      | do x
+
 -- This is called contract programming
+
+-- add :: Integer -> Integer -> Integer   --function declaration 
+-- add x y =  x + y                       --function definition 
+-- where
+--  | doc x as "Integer 1"
+--  | doc y as "Integer 2"
+--  | doc as "Function to add 2 Integers"
+--  | author as "github.com/tobybenjaminclark"
+
+-- add32 :: Integer -> Integer -> Integer32
+-- add32 x y | (add x y) < 32 -> add x y
+--           | (add x y) > 31 -> 0
+-- where
+--  | doc x as "Integer 1"
+--  | doc y as "Integer 2"
+--  | doc as "Function to add 2 Integers, output a 32 Bit Integer"
+--  | author as "Toby Benjamin Clark"
+
+-- inbuilts
+-- + - * / %
+-- > <
+-- head
+-- tail
+-- null
+-- :: (append/prepend)
+-- char -> ascii
+-- ascii -> char
+-- support tuple (fst/snd)
+
+-- add :: Integer -> Integer -> Integer   --function declaration 
+-- add x y =  x + y                       --function definition 
+-- where
+--  | doc x as "Integer 1"
+--  | doc y as "Integer 2"
+--  | doc as "Function to add 2 Integers"
+--  | author as "github.com/tobybenjaminclark
+-- end add
+
+-- func functionname :: Type -> Type -> Type ...
+-- func functionname parameters = result
+-- where
+--  | flag param as value
+--  | flag as value
+-- end functionname
