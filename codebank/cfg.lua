0c838cf57b12112f16c2896ac46ca5892987b05c
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-12 11:00:10 +0100

    add: changes to cfg.lua file

--- codebank/cfg.lua
+++ codebank/cfg.lua
@@ -4,10 +4,10 @@ letter                      ::=         'a' | 'b' | ... | 'z' | 'A' | 'B' | ... 
 digit                       ::=         '0' | '1' | ... | '9'
 
 -- function
-function_declaration        ::=         "func " function_identifier "::" function_curry
-function_identifier         ::=         ( letter | digit | symbol ) | ( identifier letter | digit | symbol)
+function_declaration        ::=         "func " function_identifier "::" function_curry '\n'
+function_identifier         ::=         ( letter | digit | symbol ) | ( identifier letter | digit | symbol) '\n'
 function_curry              ::=         ( identifier ("->"|"→") identifier | function_curry)                              -- identifier here must be a type.
 
-function_definition         ::=         "func " function_identifier parameter_list
+function_definition         ::=         "func " function_identifier parameter_list 
 parameter                   ::=         ( letter | digit | symbol ) | ( identifier letter | digit | symbol)
-parameter_list              ::=         parameter | (parameter parameter_list)
+parameter_list              ::=         parameter | (parameter ", " parameter_list) 
