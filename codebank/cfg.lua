7ed57e35d694507123ebfee34f7577effeb029be
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-13 15:29:21 +0100

    add: update cfg to include arithmetic expressions

--- codebank/cfg.lua
+++ codebank/cfg.lua
@@ -11,3 +11,16 @@ function_curry              ::=         ( identifier ("->"|"→") identifier | f
 function_definition         ::=         "func " function_identifier parameter_list 
 parameter                   ::=         ( letter | digit | symbol ) | ( identifier letter | digit | symbol)
 parameter_list              ::=         parameter | (parameter ", " parameter_list) 
+
+where_section               ::=         ("where:" | "such that:") "\n" doc_definitions "endwhere" "\n"
+doc_definitions             ::=         "|" identifier (identifier | None) " as " string/whatever? "\n" recursive more     -- do doc definitions more
+
+arithmetic_expression       ::=         term | arithmetic_expression '+' term | arithmetic_expression '-' term
+term                        ::=         factor | term '*' factor | term '/' factor
+factor                      ::=         '(' expression ')' | number | identifier | function_call
+function_call               ::=         function_name '(' argument_list ')'
+argument_list               ::=         expression | argument_list ',' expression
+number                      ::=         digit | ( number digit )
+
+variable_definition         ::=         "let " identifier " as " identifier
+variable_declaration        ::=         "set " identifier " as " arithmetic_expression
