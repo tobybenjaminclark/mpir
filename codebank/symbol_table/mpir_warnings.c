f09e6867dfb2aa4b06df97a34b4796677b780a40
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-15 14:45:22 +0100

    doc: mpir_warnings.c & mpir_warnings.h with doxygen comments

--- codebank/symbol_table/mpir_warnings.c
+++ codebank/symbol_table/mpir_warnings.c
@@ -1,34 +1,131 @@
+// Import header file.
 #include "mpir_warnings.h"
 
-// Function to log messages based on log level
+/// @brief Displays a formatted message to the specified output stream with the given prefix.
+///
+/// This function formats the input message with the provided format string and arguments, adds
+/// the specified prefix, and prints the resulting message to the specified output stream.
+/// It ensures that there is no buffer overflow and handles error cases appropriately.
+///
+/// @param stream The output stream where the message will be printed.
+/// @param prefix The prefix to be added to the formatted message.
+/// @param format The format string used for formatting the message.
+/// @param args A va_list containing the arguments to be formatted.
+///
+void display_message(FILE* stream, const char* prefix, const char* format, va_list args)
+{
+    // Declaring a buffer to store the displayed message within. After this, the message is
+    // formatted within the buffer, and checked to ensure no buffer overflow.
+    char buffer[4096]; 
+    if (vsnprintf(buffer, sizeof(buffer), format, args) >= 0)
+    {
+        // Print the formatted message to the output stream with the specified prefix
+        // Check if fprintf was successful, if not, print an error message to stderr
+        if (fprintf(stream, "%s%s\n", prefix, buffer) < 0)
+        {
+            // Display an error message indicating a buffer overflow.
+            fprintf(stderr, MESSAGE_BUFFER_OVERFLOW); 
+        }
+    }
+    else
+    {
+        // Display an error message indicating a message formatting failure.
+        fprintf(stderr, MESSAGE_FORMATTING_FAILURE); 
+    }
+}
+
+/// @brief Prints an informational message to the standard output stream.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the information prefix, and prints the resulting message to the standard output stream.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
+void mpir_info(const char *format, ...)
+{
+    // va_list is an argument list of variable length, allowing for string formatting. It
+    // is initialized with the last named parameter (format)
+    va_list args; 
+    va_start(args, format); 
+    
+    // Call the display_message function to format with the information prefix, and output
+    // to the stdout stream.
+    display_message(stdout, INFO_PREFIX, format, args);
+    va_end(args); 
+}
+
+/// @brief Prints a warning message to the standard output stream.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the warning prefix, and prints the resulting message to the standard output stream.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
 void mpir_warn(const char *format, ...)
 {
+    // va_list is an argument list of variable length, allowing for string formatting. It
+    // is initialized with the last named parameter (format)
     va_list args;
     va_start(args, format);
-    printf(WARNING_PREFIX);
-    vprintf(format, args);
-    printf("\n");
+
+    // Call the display_message function to format with the warning prefix, and output
+    // to the stdout stream.
+    display_message(stdout, WARNING_PREFIX, format, args);
     va_end(args);
 }
 
-// Function to log messages based on log level
+/// @brief Prints an error message to the standard error stream.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the error prefix, and prints the resulting message to the standard error stream.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
 void mpir_error(const char *format, ...)
 {
+    // va_list is an argument list of variable length, allowing for string formatting. It
+    // is initialized with the last named parameter (format)
     va_list args;
     va_start(args, format);
-    fprintf(stderr, ERROR_PREFIX);
-    vfprintf(stderr, format, args);
-    fprintf(stderr, "\n");
+
+    // Call the display_message function to format with the error prefix, and output
+    // to the stderr stream, indicating it is an erroneous message.
+    display_message(stderr, ERROR_PREFIX, format, args);
     va_end(args);
 }
 
-// Function to log messages based on log level
+/// @brief Prints a fatal error message to the standard error stream and terminates the program.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the fatal error prefix, and prints the resulting message to the standard error stream.
+/// After printing the message, the program is terminated.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
 void mpir_fatal(const char *format, ...)
 {
+    // va_list is an argument list of variable length, allowing for string formatting. It
+    // is initialized with the last named parameter (format)
     va_list args;
     va_start(args, format);
-    fprintf(stderr, FATAL_PREFIX);
-    vfprintf(stderr, format, args);
-    fprintf(stderr, "\n");
+
+    // Call the display_message function to format with the information prefix, and output
+    // to the stderr stream, indicating it is an erroneous message. 
+    display_message(stderr, FATAL_PREFIX, format, args);
     va_end(args);
+}
+
+/// @brief Demonstration of the error logging capability of the MPIR Compiler, displays mock-messages
+/// @return Always returns 0.
+int main() 
+{
+    mpir_info("This is an information log, use to convey information to the user.");
+    mpir_warn("This is a warning log, used to warn the user about problems.");
+    mpir_error("This is an error log, used to present errors.");
+    mpir_fatal("This is a fatal log, used to show serious errors.");
+    return 0;
 }
--- codebank/symbol_table/mpir_warnings.h
+++ codebank/symbol_table/mpir_warnings.h
@@ -2,9 +2,63 @@
 #include <stdarg.h>
 
 #define WARNING_PREFIX "MPIR_Compiler -> Warning :: "
-#define ERROR_PREFIX "MPIR_Compiler -> Error :: "
-#define FATAL_PREFIX "MPIR_Comiler -> Fatal :: "
+#define ERROR_PREFIX "MPIR_Compiler -> Error   :: "
+#define FATAL_PREFIX "MPIR_Compiler -> Fatal   :: "
+#define INFO_PREFIX "MPIR_Compiler -> Info    :: "
 
+#define MESSAGE_BUFFER_OVERFLOW "%sFailed to show message (message size exceeded allocated buffer size).\n", ERROR_PREFIX
+#define MESSAGE_FORMATTING_FAILURE "%sFailed to show message (internal error formatting message).\n", ERROR_PREFIX
+
+/// @brief Displays a formatted message to the specified output stream with the given prefix.
+///
+/// This function formats the input message with the provided format string and arguments, adds
+/// the specified prefix, and prints the resulting message to the specified output stream.
+/// It ensures that there is no buffer overflow and handles error cases appropriately.
+///
+/// @param stream The output stream where the message will be printed.
+/// @param prefix The prefix to be added to the formatted message.
+/// @param format The format string used for formatting the message.
+/// @param args A va_list containing the arguments to be formatted.
+///
+void display_message(FILE* stream, const char* prefix, const char* format, va_list args);
+
+/// @brief Prints an informational message to the standard output stream.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the information prefix, and prints the resulting message to the standard output stream.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
+void mpir_info(const char *format, ...);
+
+/// @brief Prints a warning message to the standard output stream.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the warning prefix, and prints the resulting message to the standard output stream.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
 void mpir_warn(const char *format, ...);
+
+/// @brief Prints an error message to the standard error stream.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the error prefix, and prints the resulting message to the standard error stream.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
 void mpir_error(const char *format, ...);
+
+/// @brief Prints a fatal error message to the standard error stream and terminates the program.
+///
+/// This function formats the input message with the provided format string and arguments,
+/// adds the fatal error prefix, and prints the resulting message to the standard error stream.
+/// After printing the message, the program is terminated.
+///
+/// @param format The format string used for formatting the message.
+/// @param ... Additional arguments to be formatted according to the format string.
+///
 void mpir_fatal(const char *format, ...);
