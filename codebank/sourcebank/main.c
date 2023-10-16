e2f3db43edde219af053a775fa714981f66f3d19
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-16 19:59:39 +0100

    add: restructure to support launch.json & tasks.json in vscode

---
+++ codebank/sourcebank/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    printf("Hello from the MPIR Compilerr! How are things?\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("Ansi hl shouldavederd wasarn this d... {%d}\n", i);
+    }
+
+    return 0;
+}
