7e2c5539b8ab04dc6d10b743d1401ac00961ff53
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-20 15:07:42 +0100

    add: string parsing

--- codebank/sourcebank/mpir_lexer/mpir_tokenize_function_declaration.c
+++ codebank/sourcebank/mpir_lexer/mpir_tokenize_function_declaration.c
@@ -1,5 +1,36 @@
 #include "../../headerbank/mpir_lexer/mpir_tokenize_function_declaration.h"
 
+bool mpir_consume_char(mpir_lexer* lexer, char expected)
+{
+    char input_character = fgetc(lexer->source_file);
+
+    if (input_character == expected)
+    {
+        printf("Character consumed: %c\n", input_character);
+        return true;
+    }
+    else
+    {
+        mpir_error("Lexer expected '%c' but received '%c' at line %d, column %d", expected, input_character);
+        return false;
+    }
+}
+
+bool mpir_parse_string(mpir_lexer* lexer, const char* str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (!mpir_consume_char(lexer, str[i]))
+        {
+            // Error occurred, handle it (possibly log the error and return false)
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
 bool mpir_tokenize_function_declaration(mpir_lexer* lexer)
 {
     // Do more stuff here.
