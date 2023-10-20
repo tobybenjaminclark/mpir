849d525f02f255e041301aa5b83bb76298fef107
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-20 12:54:09 +0100

    add: add malloc/free token functions.

--- codebank/sourcebank/mpir_lexer/mpir_token.c
+++ codebank/sourcebank/mpir_lexer/mpir_token.c
@@ -1,10 +1,28 @@
 #include "../../headerbank/mpir_lexer/mpir_token.h"
 
-mpir_token* mpir_create_token(mpir_token_type type, char lexeme[50], int line)
+mpir_token* mpir_create_token(mpir_token_type type, const char lexeme[50], int line)
 {
-    mpir_token* return_token;
+    // Allocate memory for the token structure
+    mpir_token* return_token = (mpir_token*)malloc(sizeof(mpir_token));
+
+    // Check if memory allocation was successful
+    if (return_token == NULL) {
+        // Handle memory allocation failure here (e.g., return an error code)
+        return NULL;
+    }
+
+    // Copy the lexeme to the token structure (assuming lexeme is a C-style string)
+    strncpy(return_token->lexeme, lexeme, sizeof(return_token->lexeme) - 1);
+    return_token->lexeme[sizeof(return_token->lexeme) - 1] = '\0'; // Null-terminate the lexeme
+
+    // Assign values to the token structure
     return_token->type = type;
-    return_token->lexeme = lexeme;
     return_token->line = line;
+
     return return_token;
+}
+
+void mpir_free_token(mpir_token* token)
+{
+    free(token);
 }
