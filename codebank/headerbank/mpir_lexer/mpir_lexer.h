6db8818cf422fda11af4ba126dbe61ef8f97ae7e
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-21 14:21:01 +0100

    doc: mpir_lexer_create in mpir_lexer.c/.h using full doxygen & inline comments.

--- codebank/headerbank/mpir_lexer/mpir_lexer.h
+++ codebank/headerbank/mpir_lexer/mpir_lexer.h
@@ -29,8 +29,27 @@ typedef struct
     char buffer[BUFFER_SIZE];           // Buffer for constructing token lexemes dynamically
 } mpir_lexer;
 
+/// @brief Attempts to create a MPIR lexer for tokenizing an .mpir input file.
+///
+/// This function initializes an MPIR lexer structure used for tokenizing a source file in the MPIR compiler.
+/// The lexer processes the specified file, breaking it down into individual tokens that are used in the
+/// subsequent stages of the MPIR compilation process. Please free this structure using ther mpir_lexer_free()
+/// function after use to avoid memory leaks.
+///
+/// @param filepath The path to the input file to be tokenized.
+///
+/// @return A pointer to the newly created MPIR lexer structure, or NULL in case of failure.
+///         If memory allocation fails, an error message is logged, and NULL is returned.
+///         If the specified file cannot be opened, an error message is logged, the allocated memory is freed, and NULL is returned.
+///
 mpir_lexer* mpir_lexer_create(const char *filepath);
+
+
+
 void mpir_lexer_free(mpir_lexer *lexer);
+
+
+
 int mpir_lexer_tokenize(mpir_lexer *lexer);
 
 #endif //MPIR_COMPILER_MPIR_LEXER_H
