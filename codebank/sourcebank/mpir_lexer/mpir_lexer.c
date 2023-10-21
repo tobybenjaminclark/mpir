5f2833cd56b1bac2e6610d418256e7b7df905c88
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-21 14:20:55 +0100

    doc: mpir_lexer_create in mpir_lexer.c/.h using full doxygen & inline comments.

--- codebank/sourcebank/mpir_lexer/mpir_lexer.c
+++ codebank/sourcebank/mpir_lexer/mpir_lexer.c
@@ -1,16 +1,31 @@
 #include "../../headerbank/mpir_lexer/mpir_lexer.h"
 
-// Function to initialize mpir_lexer struct and open the specified file
+/// @brief Attempts to create a MPIR lexer for tokenizing an .mpir input file.
+///
+/// This function initializes an MPIR lexer structure used for tokenizing a source file in the MPIR compiler.
+/// The lexer processes the specified file, breaking it down into individual tokens that are used in the
+/// subsequent stages of the MPIR compilation process. Please free this structure using ther mpir_lexer_free()
+/// function after use to avoid memory leaks.
+///
+/// @param filepath The path to the input file to be tokenized.
+///
+/// @return A pointer to the newly created MPIR lexer structure, or NULL in case of failure.
+///         If memory allocation fails, an error message is logged, and NULL is returned.
+///         If the specified file cannot be opened, an error message is logged, the allocated memory is freed, and NULL is returned.
+///
 mpir_lexer* mpir_lexer_create(const char *filepath)
 {
+    // Allocate memory for the lexer
     mpir_lexer *lexer = (mpir_lexer*)malloc(sizeof(mpir_lexer));
 
+    // Check for memory allocation failure
     if (lexer == NULL)
     {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(EXIT_FAILURE);
+        mpir_error("Out of memory, failed to allocate MPIR Lexer.");
+        return NULL;
     }
 
+    // Initialize lexer properties to their appropriate values.
     lexer->current_index = 0;
     lexer->buffer_size = 0;
     lexer->token_count = 0;
@@ -19,13 +34,15 @@ mpir_lexer* mpir_lexer_create(const char *filepath)
     lexer->current_char = '\0';
     lexer->tokens = NULL;
 
+    // Open the source file
     lexer->source_file = fopen(filepath, "r");
 
+    // Check if file opening is successful
     if (lexer->source_file == NULL)
     {
-        fprintf(stderr, "Error opening file: %s\n", filepath);
+        mpir_error("Lexer failed to open file at %s", filepath);
         free(lexer);
-        return(NULL);
+        return NULL;
     }
 
     return lexer;
