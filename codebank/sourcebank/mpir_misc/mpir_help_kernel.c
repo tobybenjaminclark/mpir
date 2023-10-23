22559c23e461348fbb1838be1aa721ab926564c3
Toby Clark <psytc9@nottingham.ac.uk>
2023-10-23 19:56:11 +0100

    add: refactor license comments from // to /* */ style in accordance with ANSI-C

--- codebank/sourcebank/mpir_misc/mpir_help_kernel.c
+++ codebank/sourcebank/mpir_misc/mpir_help_kernel.c
@@ -1,7 +1,8 @@
-
-// GPL v3 - MPIR Compiler :: Copyright (C) 2023 Toby Benjamin Clark
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-// License as published by the Free Software Foundation, of version 3 or later - See LICENSE for full terms of use.
+/*
+ * GPL v3 - MPIR Compiler :: Copyright (C) 2023 Toby Benjamin Clark. All hail Gallaxhar!
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, of version 3 or later - See LICENSE for full terms of use.
+ */
 
 #include "../../headerbank/mpir_misc/mpir_help_kernel.h"
 
