d314f416c37a4e399b5cbb5cbb9d3fbaa362b7f1
Toby Clark <psytc9@nottingham.ac.uk>
2024-02-28 21:14:58 +0000

    add: eq bounds checking to type checker

--- codebank/modulebank/mpir_typechecker/bound_check.py
+++ codebank/modulebank/mpir_typechecker/bound_check.py
@@ -1,33 +1,44 @@
 import z3
 from functools import partial
 
-def _get_combine_func(index: int):
+# Higher-Order Function that returns a min\max application dependent on the passed index.
+def _get_combine_func(index: int) -> partial:
     if index == 1:  return partial(max, default=float('-inf'))
     elif index == 2: return partial(min, default=float('inf'))
         
-
-def get_bound(t, combine_func_index, default_value):
+# Function to get the GLB/GUB of a given predicate, dependent on `combine_func_index`
+def get_bound(t: z3.Bool, combine_func_index: int, default_value: float) -> float:
     if z3.is_and(t):
         bounds = [get_bound(arg, 1 if combine_func_index == 2 else 2, default_value) for arg in t.children()]
-        return _get_combine_func(combine_func_index)(bounds) 
+        return _get_combine_func(combine_func_index)(bounds)
     elif z3.is_or(t):
         bounds = [get_bound(arg, combine_func_index, default_value) for arg in t.children()]
-        return _get_combine_func(combine_func_index)(bounds)        
+        return _get_combine_func(combine_func_index)(bounds)
     elif z3.is_lt(t) or z3.is_gt(t):
         bound_value = z3.simplify(t.children()[1])
-        return float(bound_value.as_long()) if z3.is_const(bound_value) else default_value
+        if z3.is_arith(bound_value):
+            return float(bound_value.as_long()) if z3.is_int(bound_value) else default_value
+        else:
+            return default_value
+    elif z3.is_eq(t):
+        print(z3.simplify(t.children()[0]), z3.simplify(t.children()[1]))
+        bound_value_0 = z3.simplify(t.children()[0])
+        bound_value_1 = z3.simplify(t.children()[1])
+        try: float(bound_value_0.as_long()) 
+        except: pass
+        try: return float(bound_value_1.as_long()) 
+        except: pass
+        return default_value
     else:
         return default_value
 
-def get_infimum(predicate) -> float:
-    return get_bound(predicate, 1, float('-inf'))
-
-def get_supremum(predicate) -> float:
-    return get_bound(predicate, 2, float('inf'))
+def get_infimum(predicate) -> float: return get_bound(predicate, 1, float('-inf'))
+def get_supremum(predicate) -> float: return get_bound(predicate, 2, float('inf'))
 
 # Test expression
 x = z3.Real('x')
-bool_expression = z3.And(z3.And(x > 5, 10 > x), z3.And(x > 0, 15 > x))
+# bool_expression = z3.And(z3.And(x > 5, 10 > 2), z3.And(x > 0, 15 > x))
+bool_expression = z3.And(x == 5)
 print(bool_expression)
 
 # Calculate and print the infimum
--- codebank/modulebank/mpir_typechecker/mpir_module_typecheck.py
+++ codebank/modulebank/mpir_typechecker/mpir_module_typecheck.py
@@ -51,6 +51,98 @@ def parse_json_file(filename: str) -> dict|None:
         print(f"Error decoding AST in '{filename}': {e}")
         return None
 
+from z3 import *
+
+def validate_ast(ast) -> bool:
+    return True
+
+def get_operator(operator_str):
+    operators = {"+": lambda x, y: x + y,
+                 "-": lambda x, y: x - y,
+                 "*": lambda x, y: x * y,
+                 "/": lambda x, y: x / y}
+
+    return operators.get(operator_str, None)
+
+def type_ast_expression(ast) -> bool:
+    x = Real('α')
+    match ast["TYPE"]:
+        case "EXPRESSION_NUMERICAL_LITERAL":
+            return lambda: x == ast["VALUE"]
+        case "EXPRESSION_OPERATOR":
+            return get_operator(ast["IDENTIFIER"])(type_ast_expression(ast["LEFT"]), type_ast_expression(ast["RIGHT"]))
+
+expression_dict = {
+        "TYPE": "EXPRESSION_OPERATOR",
+        "IDENTIFIER": "+",
+        "LEFT": {
+            "TYPE": "EXPRESSION_NUMERICAL_LITERAL",
+            "VALUE": 5.000000
+        },
+        "RIGHT": {
+            "TYPE": "EXPRESSION_OPERATOR",
+            "IDENTIFIER": "+",
+            "LEFT": {
+                "TYPE": "EXPRESSION_NUMERICAL_LITERAL",
+                "VALUE": 1.000000
+            },
+            "RIGHT": {
+                "TYPE": "EXPRESSION_NUMERICAL_LITERAL",
+                "VALUE": 10.000
+            }
+        }
+    }
+print(type_ast_expression(expression_dict))
+            
+
+
+class TypeCheck():
+    def __init__(self, ast:dict) -> None:
+
+        # Validate AST
+        if not validate_ast(ast): raise Exception("Invalid AST")
+        self.types: list[dict] = [node for node in ast["CONTENTS"] if node["TYPE"] == "TYPE_DECLARATION"]
+        self.types_logic: dict[str:z3] = {}
+        self.functions: list[dict] = [node for node in ast["CONTENTS"] if node["TYPE"] == "FUNCTION_DECLARATION"]
+
+        self.validate_types()
+        self.validate_functions()
+
+    def validate_functions(self) -> None:
+        for function_index, func in enumerate(self.functions):        
+            
+            # Setup Typing Context for this Function & Populate it with input types
+            # context: dict[str:z3] = {arg: self.types_logic[func["INPUTS"][index]] for index, arg in enumerate(func["ARGUMENTS"])}
+            context: dict[str:z3] = {}
+
+            for index, statement in enumerate(func["BODY"]):
+                if(statement["TYPE"] == "TYPE_ASSIGNMENT"):
+                    context[statement["IDENTIFIER"]] = statement["ASSIGNED_TYPE"]
+                    print("Type Assignment to " + statement["IDENTIFIER"] + " ( has type: " + str(context[statement["IDENTIFIER"]]) + ")")
+                if(statement["TYPE"] == "VALUE_ASSIGNMENT"):
+                    print("Value Assignment to " + statement["IDENTIFIER"] + " ( has type: " + str(context[statement["IDENTIFIER"]]) + ")")
+                    if(statement["EXPRESSION"]["TYPE"] == "EXPRESSION_NUMERICAL_LITERAL"):
+                        print("Literal!")
+                continue    
+            print(context)
+        return None
+
+    def validate_types(self) -> None:
+        
+        print("Validating Types")
+        for type_index, type in enumerate(self.types):
+
+            # Create Solver for Curernt Type
+            type_solver: z3.Solver = z3.Solver()
+            self.types_logic[type["IDENTIFIER"]] = form_expression(type_solver, type["LOGIC"])
+            type_solver.add(self.types_logic[type["IDENTIFIER"]])
+
+            # Check Solver to make ensure type satisfiability
+            if type_solver.check() == z3.sat: print(f"  → Type " + type["IDENTIFIER"] + " is satisfiable")
+            else: print(f"  → Type " + type["IDENTIFIER"] + " is unsatisfiable")
+                
+            continue
+        return None
 
 
 ast = parse_json_file("testj.json")
