e51a0430599ab03a459782d50f7827988c7d377a
Toby Clark <psytc9@nottingham.ac.uk>
2024-03-05 22:02:30 +0000

    add: getter test

--- codebank/modulebank/mpir_typechecker/test_typing_context.py
+++ codebank/modulebank/mpir_typechecker/test_typing_context.py
@@ -14,11 +14,18 @@ def test_type_function():
     assert τ.logic.output_constraint == True
     assert τ.type == type_variants._function
 
-# Function to test adding to a Typing Context.
-def test_typing_context():
+# Function to test adding to a Typing Context and fetching using container, `in`.
+def test_typing_context_container():
     Γ = context_create()
     Γ = add_type_to_context(Γ, "τ", type_create_singular(True))
     assert "τ" in Γ
+
+# Function to test adding to a Typing Context and fetching using getter.
+def test_typing_context_getter():
+    Γ = context_create()
+    τ = type_create_singular(True)
+    Γ = add_type_to_context(Γ, "τ", τ)  
+    assert get_type_from_context(Γ, "τ") == τ
 
 # Execute all defined tests.
 if __name__ == "__main__":
