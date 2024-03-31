b064a20ff09ab7cb30012f43174f53e84463627e
Toby Clark <psytc9@nottingham.ac.uk>
2024-03-31 17:06:32 +0100

    add: tidy tex generation

--- codebank/modulebank/mpir_module_tex.py
+++ codebank/modulebank/mpir_module_tex.py
@@ -1,75 +1,40 @@
 import argparse
 import json
-
 import os
 
-# get the current working directory
-current_working_directory = os.getcwd()
-
-# print output to the console
-print(current_working_directory)
-
-def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Build MPiR AST to Python3 Code')
-    parser.add_argument('arg1', type=str, help='Input File')
-    parser.add_argument('--output', type=str, help='Output File', default = "output.py")
-    return parser.parse_args()
-
-def write_arguments_to_file(filename: str, args: argparse.Namespace):
-    with open(filename, 'w') as file:
-        file.write("arg1: {}\n".format(args.arg1))
-
+# Function to parse a JSON file (ast, in this case.)
 def parse_json_file(filename: str) -> dict|None:
-    try:
-        file = open(filename, 'r')
-        data = json.load(file)
-        file.close()
-        return data
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-        return None
-    except json.JSONDecodeError as e:
-        print(f"Error decoding AST in '{filename}': {e}")
-        return None
+    try: return json.load(open(filename, 'r'))
+    except FileNotFoundError as e: print(f"File '{filename}' not found."); return None
+    except json.JSONDecodeError as e: print(f"Error decoding AST in '{filename}': {e}"); return None
 
+# Function to convert a function call to pseudocode string.
 def convert_function_call(fcall: dict) -> str:
-    val = fcall["IDENTIFIER"] + "("
-    for index, arg in enumerate(fcall["ARGUMENTS"]):
-        val = val + convert_expression(arg["VALUE"]) + (", " if index < len(fcall["ARGUMENTS"]) - 1 else "")
-    val = val + ")"
-    return val
+    return fcall["IDENTIFIER"] + "(" + ", ".join([convert_expression(arg["VALUE"]) for arg in fcall["ARGUMENTS"]]) + ")"
 
+# Function to convert an expression to pseudocode string.
 def convert_expression(expr: dict) -> str:
-    match(expr["TYPE"]):
-        case "FUNCTION_CALL":
-            return convert_function_call(expr)
-        case "EXPRESSION_IDENTIFIER":
-            return expr["IDENTIFIER"]
-        case "EXPRESSION_NUMERICAL_LITERAL":
-            try:
-                return str(expr["VALUE"])
-            except:
-                print(expr)
-        case "EXPRESSION_OPERATOR":
-            return convert_expression(expr["LEFT"]) + " " + expr["IDENTIFIER"] + " " + convert_expression(expr["RIGHT"])
-        case "EXPRESSION_STRING_LITERAL":
-            return expr["IDENTIFIER"]
-    if(expr["TYPE"] == ""):
-        pass
+    match expr["TYPE"]:
+        case "FUNCTION_CALL":                   return convert_function_call(expr)
+        case "EXPRESSION_IDENTIFIER":           return expr["IDENTIFIER"]
+        case "EXPRESSION_NUMERICAL_LITERAL":    return str(expr["VALUE"])
+        case "EXPRESSION_OPERATOR":             return convert_expression(expr["LEFT"]) + " " + expr["IDENTIFIER"] + " " + convert_expression(expr["RIGHT"])
+        case "EXPRESSION_STRING_LITERAL":       return expr["IDENTIFIER"]
+        case "":                                pass
 
-def build_docsection(node):
-    lines = []
-    if len(list(filter(lambda l: l["FLAG"] == "testcase", node["DOCSECTION"]))) > 0:
+def build_examples(node, lines):
+    if len(list(filter(lambda l: l["FLAG"] == "example", node["DOCSECTION"]))) > 0:
         lines.append("\\textbf{Test Cases}")
-        lines.append("\\begin{itemize}")
+        lines.append("\\begin{enumerate}")
         lines.append("\t\\setlength{\\itemsep}{5pt}")
         lines.append("\t\\setlength{\\parskip}{0pt}")
         lines.append("\t\\setlength{\\parsep}{0pt}")
-        for line in filter(lambda l: l["FLAG"] == "testcase", node["DOCSECTION"]):
+        for line in filter(lambda l: l["FLAG"] == "example", node["DOCSECTION"]):
             lines.append("\t\\item \\verb|" + node["IDENTIFIER"] + line["STRING"].replace("&", "\&") +"|")
-        lines.append("\\end{itemize}\n")
-    
-    node["DOCSECTION"] = list(filter(lambda l: l["FLAG"] != "testcase", node["DOCSECTION"]))
+        lines.append("\\end{enumerate}\n")
+    node["DOCSECTION"] = list(filter(lambda l: l["FLAG"] != "example", node["DOCSECTION"]))
+
+def build_general(node, lines):
     if len(node["DOCSECTION"]) > 0:
         lines.append("\\textbf{Function Information}")
         lines.append("\\begin{itemize}")
@@ -82,6 +47,11 @@ def build_docsection(node):
             else:
                 lines.append("\t\\item " + doc["STRING"].replace("&", "\&"))
         lines.append("\\end{itemize}\n")
+
+def build_docsection(node):
+    lines = []
+    build_examples(node, lines)
+    build_general(node, lines)
     return lines
 
 def show_statement(statement):
@@ -100,48 +70,45 @@ def show_statement(statement):
         # for statement2 in statement["MATCH_COMMANDS"]:
         #    show_statement(statement2)
 
+def build_tex(ast):
+    lines = []
+    lines.append("\n\\section{\\textsc{Function Declarations}}")
+
+    for node in list(filter(lambda x: x["TYPE"] == "FUNCTION_DECLARATION", ast["CONTENTS"])):
+        # Start LaTeX Segment
+        lines.append("\\clearpage")
+        lines.append("\n\\subsection{" + node["IDENTIFIER"].replace("_", "\\_") + "}")
+        print("1 ", len(lines))
+
+        lines.extend(build_docsection(node))
+
+        
+        # Print Pseudocode
+        lines.append("\\begin{minted}[mathescape, linenos, numbersep=5pt, framesep=2mm, frame=lines, fontsize=\\small]{text}")
+        output_string = "FUNCTION " + node["IDENTIFIER"] + "("
+        for index, arg in enumerate(node["ARGUMENTS"]):
+            output_string += arg + ": " + node["INPUTS"][index]
+            if index < len(node["ARGUMENTS"]) - 1:
+                output_string += ", "
+        output_string += ") -> " + node["RETURN_TYPE"] + ":"
+        lines.append(output_string)
+        print("3 ",len(lines))
+        
+        for statement in node["BODY"]:
+            lines.append("\t" + show_statement(statement))
+        lines.append("\\end{minted}\n")
+        print("4 ",len(lines))
+
+    lines.append("\n\\section{\\textsc{Type Declarations}}")
+    for node in list(filter(lambda x: x["TYPE"] == "TYPE_DECLARATION", ast["CONTENTS"])):
+        lines.append("\n\\subsection{" + node["IDENTIFIER"].replace("_", "\\_") + "}")
+        docsec = build_docsection(node)
+        lines.extend(build_docsection(node))
+
+    return lines
+
 ast = parse_json_file("testj.json")
-
-if "CONTENTS" not in ast:
-    print("CONTENTS NOT IN AST!")
-    exit(1)
-
-x = 0
-
-lines = []
-lines.append("\n\\section{\\textsc{Function Declarations}}")
-
-for node in list(filter(lambda x: x["TYPE"] == "FUNCTION_DECLARATION", ast["CONTENTS"])):
-    # Start LaTeX Segment
-    lines.append("\\clearpage")
-    lines.append("\n\\subsection{" + node["IDENTIFIER"].replace("_", "\\_") + "}")
-    print("1 ", len(lines))
-
-    lines.extend(build_docsection(node))
-
-    
-    # Print Pseudocode
-    lines.append("\\begin{minted}[mathescape, linenos, numbersep=5pt, framesep=2mm, frame=lines, fontsize=\\small]{text}")
-    output_string = "FUNCTION " + node["IDENTIFIER"] + "("
-    for index, arg in enumerate(node["ARGUMENTS"]):
-        output_string += arg + ": " + node["INPUTS"][index]
-        if index < len(node["ARGUMENTS"]) - 1:
-            output_string += ", "
-    output_string += ") -> " + node["RETURN_TYPE"] + ":"
-    lines.append(output_string)
-    print("3 ",len(lines))
-    
-    for statement in node["BODY"]:
-        lines.append("\t" + show_statement(statement))
-    lines.append("\\end{minted}\n")
-    print("4 ",len(lines))
-
-lines.append("\n\\section{\\textsc{Type Declarations}}")
-for node in list(filter(lambda x: x["TYPE"] == "TYPE_DECLARATION", ast["CONTENTS"])):
-    lines.append("\n\\subsection{" + node["IDENTIFIER"].replace("_", "\\_") + "}")
-    docsec = build_docsection(node)
-    lines.extend(build_docsection(node))
-
+lines = build_tex(ast)
 # Open file for writing
 with open('sample.tex', 'w') as file:
     # Write each line to the file
