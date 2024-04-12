f100d2c4a33b614153c2f7f6674773ef53876452
Toby Clark <psytc9@nottingham.ac.uk>
2024-04-12 21:54:46 +0100

    add: better latex formatting for literate programming

--- codebank/modulebank/mpir_typechecker/mpir_module_tex.py
+++ codebank/modulebank/mpir_typechecker/mpir_module_tex.py
@@ -132,11 +132,13 @@ def build_example_usage(node, Γ):
 
 # Builds function declaration tex
 def build_function_declaration(node, Γ):
-    lines = ["\\clearpage"]
-    lines.append("\n\\subsection{" + node["IDENTIFIER"].replace("_", "\\_") + "}")
+    lines = []
+    node["IDENTIFIER"] = node["IDENTIFIER"].replace("_", "\\_")
+    lines.append("\n\\subsection{" + node["IDENTIFIER"] + "}")
     lines.extend(build_docsection(node))
     lines.extend(build_pseudocode(node))
     lines.extend(build_example_usage(node, Γ))
+    lines.append("\\clearpage")
     return lines
 
 
@@ -144,7 +146,7 @@ def build_function_declaration(node, Γ):
 def build_pseudocode(node):
     pseudocode_lines = ["\\textbf{\\\\ Pseudocode for } \\texttt{" + node["IDENTIFIER"] + "}"]
     pseudocode_lines.append("\\begin{minted}[mathescape, linenos, numbersep=5pt, framesep=2mm, frame=lines, fontsize=\\small]{text}")
-    output_string = "FUNCTION " + node["IDENTIFIER"] + "(" + build_arguments(node) + ") -> " + node["RETURN_TYPE"] + ":"
+    output_string = "FUNCTION " + node["IDENTIFIER"].replace("\\_", "_") + "(" + build_arguments(node) + ") -> " + node["RETURN_TYPE"] + ":"
     pseudocode_lines.append(output_string)
     for statement in node["BODY"]:
         pseudocode_lines.extend("\t" + pseudo for pseudo in build_pseudocode_statement(statement))
@@ -188,7 +190,6 @@ def build_min_max_middle(name, typ) -> list[str]:
 def build_type_declarations(ast, lines, Γ):
     lines.append("\n\\section{\\textsc{Type Declarations}}")
     for node in list(filter(lambda x: x["TYPE"] == "TYPE_DECLARATION", ast["CONTENTS"])):
-        lines.append("\\clearpage")
         lines.append("\n\\subsection{" + node["IDENTIFIER"].replace("_", "\\_") + "}")
         lines.extend(build_docsection(node))
 
@@ -196,7 +197,8 @@ def build_type_declarations(ast, lines, Γ):
         lines.append("$$ " + convert_expression(node['LOGIC']).replace("a", "a") + " $$\n")
 
         lines.extend(["\\textbf{\\\\ Set Notation of } \\texttt{" + node["IDENTIFIER"] + "}\n"])
-        lines.extend(build_min_max_middle(node["IDENTIFIER"], get_type_from_context(Γ, node["IDENTIFIER"])))        
+        lines.extend(build_min_max_middle(node["IDENTIFIER"], get_type_from_context(Γ, node["IDENTIFIER"])))  
+        lines.append("\\clearpage")      
 
 # Substitute all values in an ast
 def ast_substitute(d, old_str, new_str) -> list[any]:
@@ -216,11 +218,18 @@ def build_tex(ast, output_file, Γ):
     print(ast)
     print(f"TEX: Writing to {output_file}")
 
-    ast_substitute("_", " ")
+    title = "\\title{test.mpir}"
+    author = "\\author{author.m}"
+    date = "\\date{date}"
 
-    lines = []
+    lines = ["\\documentclass{article}", "\\usepackage{graphicx}", "\\usepackage{minted}", "\\usepackage{hyperref}", "\\usepackage{url}", "\\usepackage{array}",
+             "\\usepackage{tocloft}", "\\usepackage[margin=1in, top=1.4in, headsep=33pt, bottom=1.4in]{geometry}", title, author, date, "\\begin{document}", "\\maketitle",
+             "\\clearpage", "\\tableofcontents", "\\clearpage"]
+
     build_function_declarations(ast, lines, Γ)
     build_type_declarations(ast, lines, Γ)
+
+    lines.append("\\end{document}")
 
     print(len(lines))
     with open(output_file, 'w') as output_file:
@@ -249,6 +258,7 @@ def main():
 
     ast = parse_json_file(input_file)
     lines = build_tex(ast)
+    lines = ast_substitute("_", " ")
     # Open file for writing
     with open(output_file, 'w') as file:
         for l in lines:
